cf14bd439ff5ab50e196e52c197e89e65d1f72c3
Tipwheal <674714966@qq.com>
2016-04-30 09:14:21 +0800

    SomeChangesAfterMidExam
    Simplify class LoginSystem.
    Add class IOHelper to help.

---
+++ com/tipwheal/game/IOHelper.java
@@ -0,0 +1,23 @@
+package com.tipwheal.game;
+
+import java.io.*;
+
+public class IOHelper {
+	
+	public String getInputString() {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String result = null;
+		try {
+			result = reader.readLine();
+			reader.close();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	public String getInputString(String prompt) {
+		System.out.println(prompt);
+		return getInputString();
+	}
+}
