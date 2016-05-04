8f942fff58d16738ab47b112e8f9cd95268f87e9
Tipwheal <674714966@qq.com>
2016-05-04 17:05:16 +0800

    Improvement2

--- com/tipwheal/game/Team.java
+++ com/tipwheal/game/Team.java
@@ -1,6 +1,7 @@
 package com.tipwheal.game;
 
 import java.io.*;
+import java.util.*;
 
 public class Team implements Serializable {
 	private static final long serialVersionUID = -4808909708684777814L;
@@ -17,19 +18,32 @@ public class Team implements Serializable {
 	}
 
 	public void playWith(Team another) {
-		score = 0;
-		another.score = 0;
-		for (Player player : members) {
-			player.playAGame();
-			score += player.getScore();
-		}
-		for (Player player : another.members) {
-			player.playAGame();
-			another.score += player.getScore();
+		initAGame();
+		another.initAGame();
+		Random random = new Random();
+		for (int i = 0; i < 100; i++) {
+			int index = random.nextInt(members.length);
+			double interferenceA = members[index].getDefence() * 0.1;
+			double interferenceB = another.members[index].getDefence() * 0.1;
+			double hitRateA = members[index].getAbility() * 0.1 * (1 - interferenceB);
+			double hitRateB = another.members[index].getAbility() * 0.1 * (1 - interferenceA);
+			boolean shootedA = hitRateA >= random.nextDouble();
+			boolean shootedB = hitRateB >= random.nextDouble();
+			int scoreA = shootedA ? random.nextDouble() >= 0.7 ? 3 : 2 : 0;
+			int scoreB = shootedB ? random.nextDouble() >= 0.7 ? 3 : 2 : 0;
+			members[index].addScore(scoreA);
+			another.members[index].addScore(scoreB);
 		}
 	}
 
 	public String getName() {
 		return name;
+	}
+
+	private void initAGame() {
+		score = 0;
+		for (Player p : members) {
+			p.setScore(0);
+		}
 	}
 }
